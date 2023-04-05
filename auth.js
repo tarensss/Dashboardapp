3a7c6b1f9e675c273a9ad26a3db06baad2f4b8f2
TaranS <73401526+tarensss@users.noreply.github.com>
2023-04-04 23:28:16 -0400

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,30 @@
+// auth.js
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
+
+const User = require('../models/user');
+
+exports.signUp = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ message: 'Validation failed', errors: errors.array() });
+  }
+
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email is already taken' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token
